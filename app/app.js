530e15f6d105ed748501176599f31c692688710b
Vitaly Yeromenko <druzver@qip.ru>
2016-11-13 23:18:12 +0200

    init

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+
+class App extends Component {
+
+    render(){
+        return(
+          <View>
+              <Text style={{fontSize:60}}>Hello world!!!</Text>
+          </View>
+        );
+    }
+}
+
+
+export default App
